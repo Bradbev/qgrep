301933827d3708618ce4454544d64ff783a07332
unknown <Brad@.(none)>
2010-11-12 18:42:19 -0800

    Removed libarchive

--- packages/libarchive-2.8.4/cpio/test/CMakeFiles/run_bsdcpio_test.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/run_bsdcpio_test"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/run_bsdcpio_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
--- packages/libarchive-2.8.4/tar/test/CMakeFiles/run_bsdtar_test.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/run_bsdtar_test"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/run_bsdtar_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
